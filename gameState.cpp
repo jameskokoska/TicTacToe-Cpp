b8936a03ca89a7b6e69e7f9cc69139dc4b0efaac
jameskokoska <50821962+jameskokoska@users.noreply.github.com>
2020-01-01 16:08:20 -0500

    Update gameState.cpp

--- gameState.cpp
+++ gameState.cpp
@@ -1,9 +1,4 @@
-//
-//  gameState.cpp
-//  TicTacTo
-//
-//  Created by Tarek Abdelrahman on 2019-06-07.
-//  Copyright © 2019 Tarek Abdelrahman. All rights reserved.
+
 #include <iostream>
 #include "tictactoe.h"
 #include "gameState.h"
